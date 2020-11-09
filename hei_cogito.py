a04feb7d4d8a6d52a935b1c027ce50da283f4973
Jonathan Jørgensen <jona.jorg@gmail.com>
2020-11-09 18:01:45 +0100

    Add tutorial

---
+++ hei_cogito.py
@@ -0,0 +1,14 @@
+import util
+
+# Dette er bare en liten test for å bli kjent med godkjennings-systemet vi skal bruke.
+# "hei_x" skal returnere en streng som sier "Hei Cogito" (husk små og store bokstaver), dersom den får input x="Cogito"
+
+def hei_x(x):
+    # --start--
+    pass
+    # --slutt--
+
+# Ikke endre noe utenfor --start-- og --slutt--
+
+autotest_x = input()
+print(hei_x(autotest_x))
